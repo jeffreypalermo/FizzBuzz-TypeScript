8d5a35f89e33981fcefefa21d1a6abdd451de0e4
Jeffrey Palermo <jeffrey@palermo.cc>
2013-02-20 15:02:11 -0800

    Converted Configuration.ts to TS syntax

--- JavaScriptThickClient/Js/Engine/Configuration.js
+++ JavaScriptThickClient/Js/Engine/Configuration.js
@@ -1,6 +1,19 @@
-function Configuration(replacementRules, lowerBound, upperBound) {
-    this.replacementRules = replacementRules;
-    this.lowerBound = lowerBound;
-    this.upperBound = upperBound;
-}
+var JavaScriptThickClient;
+(function (JavaScriptThickClient) {
+    (function (Js) {
+        (function (Engine) {
+            var Configuration = (function () {
+                function Configuration(replacementRules, lowerBound, upperBound) {
+                    this.replacementRules = replacementRules;
+                    this.lowerBound = lowerBound;
+                    this.upperBound = upperBound;
+                }
+                return Configuration;
+            })();
+            Engine.Configuration = Configuration;            
+        })(Js.Engine || (Js.Engine = {}));
+        var Engine = Js.Engine;
+    })(JavaScriptThickClient.Js || (JavaScriptThickClient.Js = {}));
+    var Js = JavaScriptThickClient.Js;
+})(JavaScriptThickClient || (JavaScriptThickClient = {}));
 //@ sourceMappingURL=Configuration.js.map
--- JavaScriptThickClient/Js/Engine/Configuration.ts
+++ JavaScriptThickClient/Js/Engine/Configuration.ts
@@ -1,5 +1,15 @@
-﻿function Configuration(replacementRules, lowerBound, upperBound) {
-    this.replacementRules = replacementRules;
-    this.lowerBound = lowerBound;
-    this.upperBound = upperBound;
+﻿module JavaScriptThickClient.Js.Engine {
+
+    export class Configuration {
+        private replacementRules: any;
+        private lowerBound: number;
+        private upperBound: number;
+
+        constructor(replacementRules, lowerBound, upperBound) {
+            this.replacementRules = replacementRules;
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+
+        }
+    }
 }
