e91969bbe80e3b70a35745d1f860b7ff399cafac
Jeffrey Palermo <jeffrey@palermo.cc>
2013-02-20 14:57:43 -0800

    Converted ApplicationBus to TypeScript syntax

--- JavaScriptThickClient/Js/Engine/ApplicationBus.js
+++ JavaScriptThickClient/Js/Engine/ApplicationBus.js
@@ -1,20 +1,32 @@
-/// <reference path="Handler.ts" />
-function ApplicationBus() {
-    this.listeners = [];
-    this.Add = function (listener) {
-        this.listeners.push(listener);
-    };
-    this.Send = function (event) {
-        for(var item in this.listeners) {
-            var handler = this.listeners[item];
-            console.log(handler.constructor.name + ": testing for handlability of " + event.constructor.name);
-            if(handler.CanHandle(event)) {
-                console.log(handler.constructor.name + ": sending event, " + event.constructor.name);
-                handler.Handle(event);
-            }
-        }
-    };
-}
-;
-var GlobalApplicationBus = new ApplicationBus();
+var JavaScriptThickClient;
+(function (JavaScriptThickClient) {
+    (function (Js) {
+        /// <reference path="Handler.ts" />
+        (function (Engine) {
+            var ApplicationBus = (function () {
+                function ApplicationBus() {
+                    this.listeners = [];
+                }
+                ApplicationBus.prototype.Add = function (listener) {
+                    this.listeners.push(listener);
+                };
+                ApplicationBus.prototype.Send = function (event) {
+                    for(var item in this.listeners) {
+                        var handler = this.listeners[item];
+                        console.log(handler.constructor.name + ": testing for handlability of " + event.constructor.name);
+                        if(handler.CanHandle(event)) {
+                            console.log(handler.constructor.name + ": sending event, " + event.constructor.name);
+                            handler.Handle(event);
+                        }
+                    }
+                };
+                return ApplicationBus;
+            })();
+            Engine.ApplicationBus = ApplicationBus;            
+        })(Js.Engine || (Js.Engine = {}));
+        var Engine = Js.Engine;
+    })(JavaScriptThickClient.Js || (JavaScriptThickClient.Js = {}));
+    var Js = JavaScriptThickClient.Js;
+})(JavaScriptThickClient || (JavaScriptThickClient = {}));
+var GlobalApplicationBus = new JavaScriptThickClient.Js.Engine.ApplicationBus();
 //@ sourceMappingURL=ApplicationBus.js.map
--- JavaScriptThickClient/Js/Engine/ApplicationBus.ts
+++ JavaScriptThickClient/Js/Engine/ApplicationBus.ts
@@ -1,24 +1,28 @@
 ﻿/// <reference path="Handler.ts" />
 
-function ApplicationBus() {
-    this.listeners = [];
-    this.Add = function(listener) {
-        this.listeners.push(listener);
-    };
+module JavaScriptThickClient.Js.Engine {
 
-    this.Send = function (event) {
-        for (var item in this.listeners) {
-            var handler = this.listeners[item];
+    export class ApplicationBus {
+        private listeners: any[] = [];
 
-            console.log(handler.constructor.name + 
-                ": testing for handlability of " + event.constructor.name);
-            
-            if (handler.CanHandle(event)) {
-                console.log(handler.constructor.name + ": sending event, " + event.constructor.name);
-                handler.Handle(event);
+        Add(listener: any) {
+            this.listeners.push(listener);
+        }
+
+        Send(event) {
+            for (var item in this.listeners) {
+                var handler = this.listeners[item];
+
+                console.log(handler.constructor.name +
+                    ": testing for handlability of " + event.constructor.name);
+
+                if (handler.CanHandle(event)) {
+                    console.log(handler.constructor.name + ": sending event, " + event.constructor.name);
+                    handler.Handle(event);
+                }
             }
         }
-    };
-};
+    }
+}
 
-var GlobalApplicationBus = new ApplicationBus();
+var GlobalApplicationBus = new JavaScriptThickClient.Js.Engine.ApplicationBus();
